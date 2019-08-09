0f5ba8d458a952828ab2a213877608884fc1739e
kryan1622 <krystalryan95@hotmail.co.uk>
2019-08-09 11:50:17 +0100

    pokemon-user is now a producer

--- Pokemon-user/src/main/java/com/bae/rest/Controller.java
+++ Pokemon-user/src/main/java/com/bae/rest/Controller.java
@@ -7,28 +7,39 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.jms.core.JmsTemplate;
 
+import com.bae.entities.SentUser;
 import com.bae.entities.Users;
 import com.bae.service.UserService;
 
 @RestController
 public class Controller {
 	
-	public UserService service;
+	private UserService service;
+	private JmsTemplate jmsTemplate;
+	
 	
 	@Autowired
-	public Controller(UserService service) {
+	public Controller(UserService service, JmsTemplate jmsTemplate) {
 		this.service= service;
+		this.jmsTemplate = jmsTemplate;
 	}
 	
 	@PostMapping("/createUser")
 	public Users createUser(@RequestBody Users user) {
+		sendToQueue(user);
 		return service.createUser(user);
 	}
 	
 	@GetMapping("/getPokemon/{userId}/{request}")
 	public ResponseEntity<Object> getPokemonByNumber(@PathVariable("userId") Long userId, @PathVariable("request") String request){
 		return service.checkId(userId, request);
+	}
+	
+	private void sendToQueue(Users user) {
+		SentUser userToStore = new SentUser(user);
+		jmsTemplate.convertAndSend("UserQueue", userToStore);
 	}
 
 }
